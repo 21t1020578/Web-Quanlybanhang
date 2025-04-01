557d711b5b18debac5827be29f43b915cf0bd66f
21t1020578 <21t1020578@huscc.edu.vn>
2025-04-01 23:20:03 +0700

    PP

---
+++ SV21T1020578/SV21T1020578.BusinessLayers/ProductDataService.cs
@@ -0,0 +1,30 @@
+﻿using SV21T1020578.DataLayers;
+using SV21T1020578.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV21T1020578.BusinessLayers
+{
+    public class ProductDataService
+    {
+        static ProductDataService()
+        {
+            string connectionString = Configuration.ConnectionString;
+            ProductDB = new ProductDAL(connectionString);
+        }
+        private static ProductDAL ProductDB { get; set; }
+        public static List<Product> ListProducts(string searchValue = "")
+        {
+            return ProductDB.List(searchValue: searchValue);
+        }
+
+        public static List<Product> ListProducts(out int rowCount, int page = 1, int pageSize = 0, string searchValue = "", int categoryId = 0, int supplierId = 0, decimal minPrice = 0, decimal maxPrice = 0)
+        {
+            rowCount = ProductDB.Count(searchValue, categoryId, supplierId, minPrice, maxPrice);
+            return ProductDB.List(page, pageSize, searchValue, categoryId, supplierId, minPrice, maxPrice);
+        }
+    }
+}
---
+++ SV21T1020578/SV21T1020578.DataLayers/CustomerUserAccountDAL.cs
@@ -0,0 +1,69 @@
+﻿using Dapper;
+using SV21T1020578.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SV21T1020578.DataLayers
+{
+    public class CustomerUserAccountDAL : BaseDAL
+    {
+        public CustomerUserAccountDAL(string connectionString) : base(connectionString)
+        {
+        }
+        /// <summary>
+        /// Kiểm tra thông tin đăng nhập của nhân viên đúng hay không.
+        /// Trả về null nếu thông tin đăng nhập không đúng
+        /// </summary>
+        /// <param name="username">Tên đăng nhập (email của nhân viên)</param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        public UserAccount? Authentiate(string username, string password)
+        {
+            UserAccount? data = null;
+            using (var connection = OpenConnection())
+            {
+                var sql = @"select CustomerID as UserId,
+		                    Email as UserName,
+		                    CustomerName as DisplayName,
+		                    N'' as Photo,
+		                    N'' as RoleNames
+                    from Customers
+                    where Email = @Email and Password = @Password";
+                var parameters = new
+                {
+                    Email = username,
+                    Password = password
+                };
+                data = connection.QueryFirstOrDefault<UserAccount>(sql: sql, param: parameters, commandType: System.Data.CommandType.Text);
+                connection.Close();
+            }
+            ;
+            return data;
+        }
+
+        public bool ChangePassword(string username, string oldpassword, string newpassword)
+        {
+            using (var connection = OpenConnection())
+            {
+                var sql = @"
+            UPDATE Customers
+            SET Password = @NewPassword
+            WHERE Email = @Username AND Password = @OldPassword";
+
+                var parameters = new
+                {
+                    Username = username,
+                    OldPassword = oldpassword,
+                    NewPassword = newpassword
+                };
+
+                int rowsAffected = connection.Execute(sql: sql, param: parameters, commandType: System.Data.CommandType.Text);
+                return rowsAffected > 0;
+            }
+        }
+    }
+}
+
